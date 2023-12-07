ba4ec5f3b0200f6318b77e3f16e166da983d9848
tokhirbek007 <tokhirbek007@gmail.com>
2023-12-07 18:04:24 +0500

    BEKZOD-KRASAVCHIK

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gameContainer = document.querySelector(".container"),
   cpuResult = document.querySelector(".cpu_result img"),
   result = document.querySelector(".result"),
   optionImages = document.querySelectorAll(".option_image");
+  /*BEKZOD-KRASAVCHIK*/
 
 optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
